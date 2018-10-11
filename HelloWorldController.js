8271bf60d14786617879883499418266cd1e1864
mohsen-poyanmehr <moh_phar@yahoo.com>
2018-10-12 00:17:12 +0200

    creat CRUD with angulaJs

--- HelloWorldController.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module("app", []);
-	app.controller("HelloWorldController", function($scope) {
-		$scope.message = "Hello, Mohsen3";	
-	}
-);
---
+++ MyController.js
@@ -0,0 +1,40 @@
+var Uid = 0;
+var app = angular.module("app", []);
+	app.controller("MyController", function($scope) {
+		$scope.contacts =[];
+		$scope.newContact = null;
+		$scope.save = function(){
+			if($scope.newContact.id==null){
+				Uid++;
+				$scope.newContact.id = Uid;
+				$scope.contacts.push($scope.newContact);
+			}
+			else{
+				for(var i in $scope.contacts){
+					if ($scope.contacts[i].id == $scope.newContact.id) {
+						$scope.contacts[i]=$scope.newContact;
+					}
+				}
+			}
+			$scope.newContact=null;
+		}
+
+		$scope.edit = function(id){
+			for(var i in $scope.contacts){
+				if ($scope.contacts[i].id==id) {
+					$scope.newContact= angular.copy($scope.contacts[i])
+				}
+			}
+		}
+
+		$scope.delete = function(id){
+			for(var i in $scope.contacts){
+				if ($scope.contacts[i].id==id) {
+					$scope.contacts[i].splice(i,1);
+					$scope.newContact=null;
+				}
+			}
+		}
+
+	}
+);
