a9ce702c236de893320b8d698b9f73d4bc5b8bee
mohsen-poyanmehr <moh_phar@yahoo.com>
2018-10-11 18:13:44 +0200

    test for identifier

--- HelloWorldController.js
+++ HelloWorldController.js
@@ -1,5 +1,5 @@
 var app = angular.module("app", []);
 	app.controller("HelloWorldController", function($scope) {
-		$scope.message = "Hello, Mohsen2";	
+		$scope.message = "Hello, Mohsen3";	
 	}
 );
